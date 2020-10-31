14df2f7d52fad0f2ae436b96f5b74e193f52e258
LincolnAgenor <lincolnagenor6@gmail.com>
2020-10-31 15:28:36 -0300

    tentativa-de-remover-um-veiculo

--- script.js
+++ script.js
@@ -8,17 +8,30 @@ document.getElementById("addCar").addEventListener("click", function () {
 });
 
 document.getElementById("EnviaCar").addEventListener("click", function () {
-  var lista = document.getElementsByClassName("listVeicles");
+  var placa = document.getElementById("novaPlaca");
+  var modelo = document.getElementById("novoModelo")
+  var Entrada = document.getElementById("novaEntrada")
   var novaPlaca = document.getElementById("placa").value;
   var novoCarro = document.getElementById("modelo").value;
   var novaEntrada = document.getElementById("Entrada").value;
   var div1 = document.createElement("div");
   div1.textContent = novaPlaca;
-  lista.appendChild(div1);
+  placa.appendChild(div1);
   var div2 = document.createElement("div");
   div2.textContent = novoCarro;
-  lista.appendChild(div2);
+  modelo.appendChild(div2);
   var div3 = document.createElement("div");
   div3.textContent = novaEntrada;
-  lista.appendChild(div3);
+  Entrada.appendChild(div3);
+  document.getElementById("tela3").style.display = "none";
+  document.getElementById("tela2").style.display = "block";
 });
+
+document.getElementById("removerCar2").addEventListener("click", function(){
+  document.getElementById("placa2").textContent ="";
+  document.getElementById("modelo2").textContent = "";
+  document.getElementById("entrada2").textContent = "";
+  var lista = document.getElementsByClassName('ul')[0];
+  var itens = lista.getElementsByTagName('div');
+  lista.removeChild(itens[1])
+})
