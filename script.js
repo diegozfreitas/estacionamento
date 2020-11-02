e5bd8b111d419605031758f8b6c8cc71224e357c
LincolnAgenor <lincolnagenor6@gmail.com>
2020-11-02 20:10:27 -0300

    atualizando-a-logica-do-js

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
-document.getElementById("entrar").addEventListener("click", function () {
-  document.getElementById("tela1").style.display = "none";
-  document.getElementById("tela2").style.display = "block";
-});
-document.getElementById("addCar").addEventListener("click", function () {
-  document.getElementById("tela2").style.display = "none";
-  document.getElementById("tela3").style.display = "block";
+document.querySelector("#tela1").style.display = "flex";
+
+const park = {
+  veicle: [],
+};
+
+puxaVeiculos = () => {
+  park.veicle.map((veicle) => {
+    document.querySelector(".listVeicles").innerHTML += `
+      <div class="veicle ${veicle.id % 2 ? "zebra" : ""} ">
+        <div class="placa">${veicle.placa}</div>
+        <div>${veicle.modelo}</div>
+        <div>${veicle.entrada}</div>
+        <div class="actions">
+          <button id ="removerCar">
+            <i class="fas fa-chevron-circle-right"></i>
+          </button>
+          <button id="EditCar">
+            <i class="fas fa-edit"></i>
+          </button>
+        </div>
+      </div>
+    `;
+  });
+};
+
+document.querySelector("#entrar").addEventListener("click", () => {
+  document.querySelector("#tela1").style.display = "none";
+  document.querySelector("#tela2").style.display = "flex";
+  puxaVeiculos();
 });
 
+document.querySelector("#addCar").addEventListener("click", () => {
+  document.querySelector("#tela2").style.display = "none";
+  document.querySelector("#tela3").style.display = "flex";
+})
+document.querySelector("#EnviaCar").addEventListener("click",(event)=>{
+  event.preventDefault();
 
-document.getElementById("EnviaCar").addEventListener("click", function () {
-  var placa = document.getElementById("placa").value;
-  var modelo = document.getElementById("modelo").value;
-  var Entrada = document.getElementById("Entrada").value;
-  
-  var carro=[placa,modelo,Entrada]
-  var estacionamento =document.getElementById("novoCarro")
-  var item ;
-  carro.forEach(n => {
-    item = document.createElement('div')
-    item.textContent = n
-    estacionamento.appendChild(item)
-  });  
-  document.getElementById("tela2").style.display = "block";
-  document.getElementById("tela3").style.display = "none";
-});
+  if(document.querySelector("#tela3").querySelector("input#placa").value == "" ||
+  document.querySelector("#tela3").querySelector("input#modelo").value == "" || 
+  document.querySelector("#tela3").querySelector("input#entrada").value =="" ){ 
 
-document.getElementById("removerCar2").addEventListener("click", function () {
-  var carro = document.getElementById("veiclezebra");
-  var parent = carro.parentNode;
-  parent.removeChild(carro);
+    alert("todos os campos são obrigatorios")
+  }
+  var placaform =document.querySelector("#tela3").querySelector("input#placa").value;
+  var modeloform = document.querySelector("#tela3").querySelector("input#modelo").value;
+  var entradaform =document.querySelector("#tela3").querySelector("input#entrada").value;
+
+  park.veicle.push({placa : placaform , modelo : modeloform , entrada : entradaform})
+
+  document.querySelector("#tela3").style.display = "none";
+  document.querySelector("#tela2").style.display = "flex";
+
+  puxaVeiculos();
+})
+park.veicle.forEach((item) => {
+  var duplicated  = reduced.findIndex(redItem => {
+      return item.a == redItem.a;
+  }) > -1;
+
+  if(!duplicated) {
+      reduced.push(item);
+  }
 });
