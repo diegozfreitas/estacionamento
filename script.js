14de24370ead814e5d1d1336adade04734a3b3ef
LincolnAgenor <lincolnagenor6@gmail.com>
2020-11-01 23:00:43 -0300

    mudando-a-forma-de-estacionar-outro-veiculo

--- script.js
+++ script.js
@@ -7,28 +7,26 @@ document.getElementById("addCar").addEventListener("click", function () {
   document.getElementById("tela3").style.display = "block";
 });
 
+
 document.getElementById("EnviaCar").addEventListener("click", function () {
-  var placa = document.getElementById("novaPlaca");
-  var modelo = document.getElementById("novoModelo")
-  var Entrada = document.getElementById("novaEntrada")
-  var novaPlaca = document.getElementById("placa").value;
-  var novoCarro = document.getElementById("modelo").value;
-  var novaEntrada = document.getElementById("Entrada").value;
-  var div1 = document.createElement("div");
-  div1.textContent = novaPlaca;
-  placa.appendChild(div1);
-  var div2 = document.createElement("div");
-  div2.textContent = novoCarro;
-  modelo.appendChild(div2);
-  var div3 = document.createElement("div");
-  div3.textContent = novaEntrada;
-  Entrada.appendChild(div3);
-  document.getElementById("tela3").style.display = "none";
+  var placa = document.getElementById("placa").value;
+  var modelo = document.getElementById("modelo").value;
+  var Entrada = document.getElementById("Entrada").value;
+  
+  var carro=[placa,modelo,Entrada]
+  var estacionamento =document.getElementById("novoCarro")
+  var item ;
+  carro.forEach(n => {
+    item = document.createElement('div')
+    item.textContent = n
+    estacionamento.appendChild(item)
+  });  
   document.getElementById("tela2").style.display = "block";
+  document.getElementById("tela3").style.display = "none";
 });
 
-document.getElementById("removerCar2").addEventListener("click", function(){
-  var carro = document.getElementById("veicle zebra");
+document.getElementById("removerCar2").addEventListener("click", function () {
+  var carro = document.getElementById("veiclezebra");
   var parent = carro.parentNode;
-  parent.removeChild(carro); 
-})
+  parent.removeChild(carro);
+});
