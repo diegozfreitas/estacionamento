1845876d560e7a4b9c8c5ddc40890d273ff15093
LincolnAgenor <lincolnagenor6@gmail.com>
2020-11-17 14:02:29 -0300

    edicao-de-veiculos-do-video

--- script.js
+++ script.js
@@ -7,22 +7,42 @@ const park = {
 
 puxaVeiculos = () => {
   // função que escreve as informações do veiculo no html
+  document.querySelector(".listVeicles").innerHTML = "";
   park.veicle.map((veicle) => {
     document.querySelector(".listVeicles").innerHTML += `
       <div class="veicle ${veicle.id % 2 ? "zebra" : ""} ">
         <div class="placa">${veicle.placa}</div>
         <div class="modelo">${veicle.modelo}</div>
-        <div class="entrada">${veicle.entrada}</div>
+        <div>${veicle.entrada}</div>
         <div class="actions">
           <button onclick="removerCarro()" id ="removerCar">
             <i class="fas fa-chevron-circle-right"></i>
           </button>
-          <button onclick="editarCarro()" id="EditCar">
+          <button id="EditCar">
             <i class="fas fa-edit"></i>
           </button>
         </div>
       </div>
     `;
+  });
+  var BTsEdit = document.querySelectorAll("#EditCar");
+
+  BTsEdit.forEach((bt) => {
+    bt.addEventListener("click", () => {
+      var element = bt.parentElement.parentElement;
+
+      document.querySelector("#tela2").style.display = "none";
+      document.querySelector("#tela3").style.display = "flex";
+
+      document.querySelector("input#placa").value = element.querySelector(
+        ".placa"
+      ).textContent;
+      document.querySelector("input#modelo").value = element.querySelector(
+        ".modelo"
+      ).textContent;
+
+      document.querySelector("form button#EnviaCar").textContent = "atualizar";
+    });
   });
 };
 
@@ -30,12 +50,17 @@ document.querySelector("#entrar").addEventListener("click", () => {
   // função que muda o display da tela 1 e 2
   document.querySelector("#tela1").style.display = "none";
   document.querySelector("#tela2").style.display = "flex";
+
+  puxaVeiculos();
 });
 
 document.querySelector("#addCar").addEventListener("click", () => {
   // função que muda o display da tela 2 e 3
   document.querySelector("#tela2").style.display = "none";
   document.querySelector("#tela3").style.display = "flex";
+
+  document.querySelector("#tela3").querySelector("input#placa").value = "";
+  document.querySelector("#tela3").querySelector("input#modelo").value = "";
 });
 document.querySelector("#EnviaCar").addEventListener("click", (event) => {
   // função que coleta as informações que serão atribuidas ao veiculo
@@ -43,9 +68,7 @@ document.querySelector("#EnviaCar").addEventListener("click", (event) => {
 
   if (
     document.querySelector("#tela3").querySelector("input#placa").value == "" ||
-    document.querySelector("#tela3").querySelector("input#modelo").value ==
-      "" ||
-    document.querySelector("#tela3").querySelector("input#entrada").value == ""
+    document.querySelector("#tela3").querySelector("input#modelo").value == ""
   ) {
     alert("todos os campos são obrigatorios");
   }
@@ -54,23 +77,32 @@ document.querySelector("#EnviaCar").addEventListener("click", (event) => {
   var modeloform = document
     .querySelector("#tela3")
     .querySelector("input#modelo").value;
-  var entradaform = document
-    .querySelector("#tela3")
-    .querySelector("input#entrada").value;
 
-  park.veicle.push({
-    placa: placaform,
-    modelo: modeloform,
-    entrada: entradaform,
-  });
+  if(park.veicle.find(veicle => veicle.placa === placaform)){
+    park.veicle.find(veicle => veicle.placa === placaform).placa = placaform;
+    park.veicle.find(veicle => veicle.placa === placaform).modelo = modeloform;
+    console.log("edita")
+  }else{
+    console.log("guarda")
+    park.veicle.push({
+      placa: placaform,
+      modelo: modeloform,
+    });
+  }
 
   document.querySelector("#tela3").style.display = "none"; //mudança no display da tela 3 e da tela 2
   document.querySelector("#tela2").style.display = "flex";
 
-
   puxaVeiculos();
 
-  console.log(park.veicle)
+  console.log(park.veicle);
+});
+
+document.querySelector("#Cancelar").addEventListener("click", (cancel) => {
+  cancel.preventDefault();
+
+  document.querySelector("#tela3").style.display = "none";
+  document.querySelector("#tela2").style.display = "flex";
 });
 
 function removerCarro() {
@@ -78,30 +110,5 @@ function removerCarro() {
   var pai = document.querySelector(".listVeicles");
   var ve = document.querySelector(".veicle");
   pai.removeChild(ve);
-  console.log(park.veicle)
+  console.log(park.veicle);
 }
-
-function editarCarro(){
-
-  removerCarro();
-
-  document.querySelector("#tela2").style.display = "none";
-  document.querySelector("#tela3").style.display = "flex";
-
-  var placaform2 = document.querySelector("#tela3").querySelector("input#placa").value;
-  var modeloform2 = document.querySelector("#tela3").querySelector("input#modelo").value;
-  var entradaform2 = document.querySelector("#tela3").querySelector("input#entrada").value;
-
-  console.log(park.veicle)
-
-  document.querySelector("#EnviaCar").addEventListener("click", () => {
-
-    park.veicle.forEach(item => {
-      item.placa = placaform2;
-      item.modelo = modeloform2;
-      item.entrada = entradaform2;
-    });
-    console.log(park.veicle)
-  })
-
-} 
